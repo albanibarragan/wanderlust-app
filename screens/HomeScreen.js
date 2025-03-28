8dc0f04f844b3b2cd0254f1aed7c8d414d5e34f0
albani <155846628+albanibarragan@users.noreply.github.com>
2025-03-28 19:52:53 -0400

    Cambiar vista Explore/Following

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,64 @@
-import { StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import HeaderWanderlust from '../components/HeaderWanderlust';
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderWanderlust from "../components/HeaderWanderlust";
 
 export default function HomeScreen({ navigation }) {
+  const [activeTab, setActiveTab] = useState("explore");
+
   return (
-    <SafeAreaView  style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <HeaderWanderlust />
-      <Text>Home Screen</Text>
-    </SafeAreaView >
+      <View style={styles.tabs}>
+        <TouchableOpacity onPress={() => setActiveTab("explore")}>
+          <Text
+            style={[styles.tab, activeTab === "explore" && styles.activeTab]}
+          >
+            Explore
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setActiveTab("following")}>
+          <Text
+            style={[styles.tab, activeTab === "following" && styles.activeTab]}
+          >
+            Following
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        {activeTab === "explore" ? (
+          <Text>Explore Content</Text>
+        ) : (
+          <Text>Following Content</Text>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+  },
+  tabs: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 5,
+    borderBottomWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#888',
+        paddingBottom: 4,
+  },
+  tab: {
+    fontSize: 16,
+    color: '#888',
+  },
+  activeTab: {
+    fontWeight: "bold",
+    color: "#000",
+    borderBottomWidth: 1,
+    borderColor: '#000',
+    paddingBottom: 4,
   },
 });
