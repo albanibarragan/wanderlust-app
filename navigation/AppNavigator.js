bdc39ded22af7f5cba6f3d0ac9d495b30f314587
albani <155846628+albanibarragan@users.noreply.github.com>
2025-03-29 21:39:15 -0400

    "Fucionando los cambios del Feed"

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,28 +1,31 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CreatePostScreen from "../screens/CreatePostScreen";
 import HomeScreen from "../screens/HomeScreen";
 import LikesScreen from "../screens/LikesScreen";
-import OnboardingScreen1 from '../screens/OnboardingScreen1';
-import OnboardingScreen2 from '../screens/OnboardingScreen2';
+import OnboardingScreen1 from "../screens/OnboardingScreen1";
+import OnboardingScreen2 from "../screens/OnboardingScreen2";
 import ProfileScreen from "../screens/ProfileScreen";
 import SearchScreen from "../screens/SearchScreen";
-import Splash from '../screens/SplashScreen';
+import Splash from "../screens/SplashScreen";
 
 const Stack = createNativeStackNavigator();
-const Tab =  createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-function MainTabs(){
-    return(
-        <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Likes" component={LikesScreen} />
-            <Tab.Screen name="Create" component={CreatePostScreen} />
-            <Tab.Screen name="Search" component={SearchScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-    );
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Likes" component={LikesScreen} />
+      <Tab.Screen name="Create" component={CreatePostScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
 }
 
 export default function AppNavigator() {
@@ -39,17 +42,8 @@ export default function AppNavigator() {
         <Stack.Screen name="Recover" component={Recover} />
         <Stack.Screen name="RecoverPassword" component={RecoverPassword} />
         <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Main" component={MainTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-    return(
-        <NavigationContainer> 
-           <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Splash" component={Splash} />
-            <Stack.Screen name="OnboardingScreen1" component={OnboardingScreen1} />
-            <Stack.Screen name="OnboardingScreen2" component={OnboardingScreen2} />
-            <Stack.Screen name="Main" component={MainTabs} />
-           </Stack.Navigator>
-        </NavigationContainer>
-    )
 }
