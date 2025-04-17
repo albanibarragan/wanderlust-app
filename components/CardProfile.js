8e422af743863cb2ea22e12c3fa8d2b7976064b6
AlbaniBarragan <155846628+albanibarragan@users.noreply.github.com>
2025-04-17 12:53:11 -0400

    Vista del perfil y card del perfil de usuario lista

---
+++ components/CardProfile.js
@@ -0,0 +1,91 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import StatsProfile from "./StatsProfile";
+import ProfileTabs from "./ProfileTabs";
+
+export default function CardProfile({
+  avatar,
+  name,
+  username,
+  bio,
+  showStats = false,
+  showTabs = false,
+  showSettings = false,
+}) {
+  return (
+    <View style={styles.container}>
+      {/* Perfil básico */}
+      <Image source={{ uri: avatar }} style={styles.avatar} />
+
+      <View style={styles.containerUser}>
+        <Text style={styles.name}>{name.trim()}</Text>
+        <Text style={styles.username}>@{username.trim()}</Text>
+
+        {/* Biografía */}
+        {bio?.trim() !== "" && (
+          <Text style={styles.bio}>{bio.trim()}</Text>
+        )}
+      </View>
+
+      {/* Sección de contenido extra */}
+      <View style={styles.extraContent}>
+        {showStats && <StatsProfile />}
+        {showTabs && <ProfileTabs />}
+        {showSettings && (
+          <View style={styles.section}>
+            <Text style={styles.settings}>Settings</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 32,
+    paddingBottom: 16,
+  },
+  avatar: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    backgroundColor: "#eee",
+    marginBottom: 12,
+  },
+  containerUser: {
+    alignItems: "center",
+    marginBottom: 8,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#111",
+  },
+  username: {
+    fontSize: 14,
+    color: "#666",
+    marginTop: 2,
+  },
+  bio: {
+    fontSize: 13,
+    color: "#444",
+    textAlign: "center",
+    marginTop: 8,
+    marginHorizontal: 24,
+    lineHeight: 18,
+  },
+  extraContent: {
+    width: "100%",
+    marginTop: 12,
+  },
+  section: {
+    marginTop: 16,
+    alignItems: "center",
+  },
+  settings: {
+    fontSize: 14,
+    color: "#007aff",
+  },
+});
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,63 @@
-import { StyleSheet, Text } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import HeaderWanderlust from '../components/HeaderWanderlust';
+import CardProfile from '../components/CardProfile';
+import { useState } from 'react';
+import ProfileTabs from '../components/ProfileTabs';
 
 export default function ProfileScreen({ navigation }) {
+  const [activeTab, setActiveTab] = useState("posts");
+  const postsUser = [
+    { id: "1", image: "https://picsum.photos/200/300" },
+    { id: "2", image: "https://picsum.photos/201/300" },
+    { id: "3", image: "https://picsum.photos/202/300" },
+    { id: "4", image: "https://picsum.photos/203/300" },
+    { id: "5", image: "https://picsum.photos/200/300" },
+    { id: "6", image: "https://picsum.photos/201/300" },
+    { id: "7", image: "https://picsum.photos/202/300" },
+    { id: "8", image: "https://picsum.photos/203/300" },
+    { id: "9", image: "https://picsum.photos/200/300" },
+    { id: "10", image: "https://picsum.photos/201/300" },
+    { id: "11", image: "https://picsum.photos/202/300" },
+    { id: "12", image: "https://picsum.photos/203/300" },
+  ];
+
+  const user = {
+    avatar: "https://randomuser.me/api/portraits/men/75.jpg",
+    name: "Lucas Scott",
+    username: "lucasscott3",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt #hashtag",
+    stats: {
+      posts: 3,
+      followers: 21,
+      following: 10,
+    },
+  };
+
   return (
     <SafeAreaView  style={styles.container}>
       <HeaderWanderlust />
-      <Text>ProfileScreen</Text>
+      <CardProfile
+        avatar={user.avatar}
+        name={user.name}
+        username={user.username}
+        bio={user.bio}
+        stats={user.stats}
+        showTabs = {true}
+      >
+      </CardProfile>
+      <View style={styles.content}>
+        {activeTab === "posts" && (
+          <FlatList
+            data={postsUser}
+            keyExtractor={(item) => item.id}
+            numColumns={2}
+            renderItem={({ item }) => (
+              <Image source={{ uri: item.image }} style={styles.postImage} />
+            )}
+          />
+        )}
+        </View>
     </SafeAreaView >
   );
 }
@@ -15,5 +66,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 8,
+    marginTop: 10,
+  },
+  postImage: {
+    width: "48%",
+    aspectRatio: 1,
+    margin: 8,
+    borderRadius: 16,
+    overflow: "hidden",
+    backgroundColor: "#fff",
+    
   },
 });
